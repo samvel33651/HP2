81e958bce09822defb2de903f7def7749f82597c
hrachya2210 <avetisyan.hrachya1991@gmail.com>
2019-07-24 20:46:42 +0400

    have chanegd comics-list  now getting  data from server

--- src/components/comics-list/comics-list.js
+++ src/components/comics-list/comics-list.js
@@ -1,28 +1,46 @@
 import  React, { Component } from 'react';
+import MapiService from '../../services/MapiService';
 
 import  './comics-list.css';
 
 class ComicsList extends Component {
+  constructor(){
+    super();
+    this.mapiService = new MapiService();
+    this.state= {
+      comics:[]
+    }
+    console.log('constructore()')
+  }
+
+  componentDidMount(){
+    this.mapiService.getAllComics()
+      .then((comics) => {
+        console.log(comics);
+        this.setState({
+          comics: comics
+        });
+      })
+  }
+
+  renderComics() {
+    const  {comics} = this.state;
+    return comics.map((comic) => {
+      return (
+        <div className="col-md-3 text-center" key={comic.id}>
+          <img src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`} className="rounded commic-img" alt="..."/>
+          <h6 className="text-info"><a href={comic.urls[0].url} target="_blank">{comic.title}</a> </h6>
+        </div>
+      );
+    });
+  }
+
   render() {
+    const  comics = this.renderComics();
     return (
       <div className="comics-list">
       <h2> Comics Page</h2>
-        <div className="col-md-3 text-center">
-          <img src="https://i.annihil.us/u/prod/marvel/i/mg/8/a0/5d2c9d011d5a2/portrait_uncanny.jpg" class="rounded" alt="..."/>
-          <h6 className="text-info">True Believers: Absolute Carnage - Mania #1 </h6>
-        </div>
-        <div className="col-md-3 text-center">
-          <img src="https://i.annihil.us/u/prod/marvel/i/mg/4/b0/577d34f19e959/portrait_uncanny.jpg" class="rounded" alt="..."/>
-          <h6 className="text-info">Psylocke #1 </h6>
-        </div>
-        <div className="col-md-3 text-center">
-          <img src="https://i.annihil.us/u/prod/marvel/i/mg/e/a0/56bb60aa7bdc2/portrait_uncanny.jpg" class="rounded" alt="..."/>
-          <h6 className="text-info">MARVEL AVENGERS ALLIANCE (2016) #4 </h6>
-        </div>
-        <div className="col-md-3 text-center">
-          <img src="https://i.annihil.us/u/prod/marvel/i/mg/9/00/5d2c927bc061c/portrait_uncanny.jpg" class="rounded" alt="..."/>
-          <h6 className="text-info">Fearless #1 </h6>
-        </div>
+        {comics}
       </div>
     );
   }
